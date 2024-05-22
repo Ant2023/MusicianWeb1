8ecec1c8e6cf24a48569ff2ac6c754b61ca9a7f5
Ant2023 <anita.ismail91@gmail.com>
2024-05-22 15:05:00 -0600

    first Commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const menu_items = document.querySelectorAll('nav .mainMenu li a');
+
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+// close menu when you click on a menu item 
+menu_items.forEach(item => {
+    item.addEventListener('click',function(){
+        close();
+    })
+})
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
+
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
+
+
+  $(document).ready(function(){
+    $('.image-slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true,
+        arrows: true,
+        infinite: false,
+    });
+});
